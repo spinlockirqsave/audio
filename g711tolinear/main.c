526ef526bbf02030398cb8927f89c752a41c736b
Piotr Gregor <piotr@dataandsignal.com>
2018-03-18 14:34:24 +0000

    Add option for A-Law/U-Law Add option for endiannes swapping. Change name of the program to g7112lin. Add install target.

--- g711tolinear/main.c
+++ g711tolinear/main.c
@@ -1,8 +1,9 @@
 /*
  * @file    main.c
- * @author  Piotr Gregor <piotrek.gregor gmail com>
- * @brief   
-*/
+ * @author  Piotr Gregor <piotr@dataandsignal.com>
+ * @brief   Conversion from G711 A-Law/U-Law to 16 bit linear.
+ * @date	18 March 2018
+ */
 
 
 #include <stdio.h>
@@ -14,83 +15,126 @@
 
 #define BUF_SZ 400
 
-int alaw2linear(int	a_val);
-
 
 void
 usage(const char *name)
 {
-    fprintf(stderr, "Usage:\n");
-    fprintf(stderr, "%s <input file> <output file>\n\n", name);
+	fprintf(stderr, "Usage:\n");
+	fprintf(stderr, "%s <input encoding> <input file> <output file> [s]\n\n", name);
+	fprintf(stderr, "For A-Law encoded input use 'a', for U-Law use 'u'.\n\nExample:\n", name);
+	fprintf(stderr, "\t%s a in.alaw out.raw\n\ns is optional, put it at the end of command if endiannes should be swapped.\n\n", name);
+	fprintf(stderr, "Example:\n\t%s a in.alaw out.raw s\n\n", name);
 }
 
 int main(int argc, char *argv[])
 {
-    FILE    *fpin = NULL, *fpout = NULL;
-    int16_t buf[BUF_SZ]= {0};
-    int8_t val = 0;
+	FILE    *fpin = NULL, *fpout = NULL;
+	char enc = 'a';
+	char swap = '0';
+	int16_t buf[BUF_SZ]= {0};
+	int8_t val = 0;
 	int16_t s = 0;
 	size_t	read = 0, read_now = 0, count = 0;
 	size_t items_n = 0;
 	size_t i = 0;
 
 
-    if (argc < 3) {
-        fprintf(stderr, "Program takes 2 arguments (input and output files).\n\n");
-        usage(argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc < 4 || argc > 5) {
+		fprintf(stderr, "\nProgram takes 3 or 4 arguments (encoding, input, output file, optional swap).\n\n");
+		goto g7112linhelp;
+	}
 
-    fpin = fopen(argv[1], "r");
-    if (fpin == NULL) {
-        fprintf(stderr, "Error opening input file.\nPlease check the file name.\n\n");
-        usage(argv[0]);
-        return EXIT_FAILURE;
-    }
+	enc = *argv[1];
+	if (enc != 'a' && enc != 'u') {
+		fprintf(stderr, "\nError. Encoding should be 'a' for A-Law, 'u' for U-Law.\n\n");
+		goto g7112linhelp;
+	}
 
-    fpout = fopen(argv[2], "w");
-    if (fpout == NULL) {
-        fprintf(stderr, "Error opening output file.\nPlease check the file name.\n\n");
-        usage(argv[0]);
-        return EXIT_FAILURE;
-    }
+	fpin = fopen(argv[2], "r");
+	if (fpin == NULL) {
+		fprintf(stderr, "\nError opening input file.\nPlease check the file name.\n\n");
+		goto g7112linhelp;
+	}
 
-    while ((read_now = fread((void *) buf, sizeof(buf[0]), BUF_SZ, fpin)) > 0) {
+	fpout = fopen(argv[3], "w");
+	if (fpout == NULL) {
+		fprintf(stderr, "\nError opening output file.\nPlease check the file name.\n\n");
+		goto g7112linhelp;
+	}
 
-		/* swap endiannes */
-		i = 0;
-/*		while (i < read_now) {
-			buf[i] = ntohs(buf[i]);
-			++i;
-		}*/
-		read += read_now;	/* in items */
+	if (argc == 5) { 
+		swap = *argv[4];
+		if (swap != 's') {
+			fprintf(stderr, "\nError. The last argument should be 's' if endiannes swapping required.\n\n");
+			goto g7112linhelp;
+		}
+	}
 
-		i = 0;				/* in items */
+	while ((read_now = fread((void *) buf, sizeof(buf[0]), BUF_SZ, fpin)) > 0) {
+
+		/**
+		 * Optionally swap endiannes.
+		 */
+
+		if (swap == 's') {
+
+			i = 0;
+
+			while (i < read_now) {
+				buf[i] = htons(buf[i]);
+				++i;
+			}
+		}
+
+		read += read_now;						/* in items */
+
+		i = 0;									/* in items */
 
 		while (i < read_now * 2) {
 
 			val = * ((char *) buf + i);
-			//s = alaw2linear(val);
-			s = alaw_to_linear(val);
+
+			if (enc == 'a') {		
+				s = alaw_to_linear(val);
+			} else {
+				s = ulaw_to_linear(val);
+			}
 
 
 			if (fwrite(&s, sizeof(s), 1, fpout) != 1) {
-				goto err;
+				goto g7112linerr;
 			}
 
 			++count;
 			++i;
 		}
-    }
+	}
 
 	fclose(fpin);
 	fclose(fpout);
 
-	fprintf(stderr, "Done: %zu samples written, %zu/%zu bytes read/written\n\n", count, read * 2, read * 4);
+	fprintf(stderr, "\nDone. ");
 
-    return EXIT_SUCCESS;
+	if (enc == 'a') {
+		fprintf(stderr, "A-Law to 16 bit linear.\n\n");
+	} else {
+		fprintf(stderr, "U-Law to 16 bit linear.\n\n");
+	}
 
-err:
+	fprintf(stderr, "%zu samples written, %zu/%zu bytes read/written\n\n", count, read * 2, read * 4);
 
+	if (swap == 's') {
+		fprintf(stderr, "Endiannes swapped.\n\n");
+	}
+
+
+	return EXIT_SUCCESS;
+
+g7112linhelp:
+	usage(argv[0]);
+	return EXIT_FAILURE;
+
+g7112linerr:
+	fprintf(stderr, "\nI/O g7112linerror while writing to %s.\n", argv[3]);
 	return EXIT_FAILURE;
 }
